c4fe10ca486c226e1e35cea007f2a5a4bdfe460e
Euclides Dry's PC Home <euclidesdry@gmail.com>
2021-02-17 01:12:51 +0100

    Learning React - Theme Switcher and Context API

---
+++ learning-react-theme-switcher-and-context-api/src/App.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-undef */
+import React, {Component} from 'react';
+import {ThemeProvider} from 'styled-components';
+
+import TodoList from './components/TodoList';
+import ThemeSwitcher from './components/ThemeSwitcher';
+
+import * as themes from './components/styles/themes';
+import ThemeContext from './components/styles/themes/context';
+
+class  App extends Component {
+  state = {
+    theme: themes.dark,
+  };
+
+  toggleTheme = () => {
+    this.setState({
+      theme: this.state.theme === themes.dark ? themes.light : themes.dark
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <ThemeContext.Provider value={this.state}>
+          <ThemeSwitcher toggleTheme={this.toggleTheme}/>
+          <ThemeContext.Consumer>
+            {theme =>
+              <ThemeProvider theme={theme}>
+                <TodoList/>
+              </ThemeProvider>
+            }
+          </ThemeContext.Consumer>
+        </ThemeContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ learning-react-theme-switcher-and-context-api/src/components/ThemeSwitcher/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ThemeSwitcher = ({toggleTheme}) => (
+    <button onClick={toggleTheme}>Alterar Tema</button>
+);
+
+export default ThemeSwitcher;
